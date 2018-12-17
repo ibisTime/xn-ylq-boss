3c3a0d8077623670976a54fb5f82c77f569464ab
hss1994 <1070798770@qq.com>
2018-12-17 10:22:22 +0800

    研发取消报告跳转

--- static/js/app/presentation/left.js
+++ static/js/app/presentation/left.js
@@ -47,13 +47,13 @@ $(function() {
                 }
             })
         ).then(function (data, data2) {
-            if (data && data.result) {
-                $("#reportLeft li.alipay a").attr('href', 'https://tenant.51datakey.com/alipay/report_data?data=' + data.message);
-            }
+            // if (data && data.result) {
+            //     $("#reportLeft li.alipay a").attr('href', 'https://tenant.51datakey.com/alipay/report_data?data=' + data.message);
+            // }
 
-            if (data2 && data2.result) {
-                $("#reportLeft li.carrierOperator a").attr('href', 'https://tenant.51datakey.com/carrier/report_data?data='+ data2.message);
-            }
+            // if (data2 && data2.result) {
+            //     $("#reportLeft li.carrierOperator a").attr('href', 'https://tenant.51datakey.com/carrier/report_data?data='+ data2.message);
+            // }
         });
         //导航切换
         $("#reportLeft li").click(function(e){
