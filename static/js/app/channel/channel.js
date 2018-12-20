025d648a32a9b524279343b8d7838791933eec33
Clare29- <1397452342@qq.com>
2018-12-20 18:01:45 +0800

    Clare29-

--- static/js/app/channel/channel.js
+++ static/js/app/channel/channel.js
@@ -5,7 +5,8 @@ $(function () {
         checkbox: true
     }, {
         field: "name",
-        title: "名称"
+        title: "名称",
+        search: true
     }, {
         field: "url",
         title: "链接"
@@ -15,13 +16,41 @@ $(function () {
     }, {
       field: "pointCount",
       title: "链接点击数"
+    }, {
+      field: "status",
+      title: "状态",
+      type: 'select',
+      search: 'true',
+      key: 'way_status',
+      formatter: Dict.getNameForList('way_status'),
     }];
     buildList({
         columns: columns,
         deleteCode: '623151',
-        pageCode: '623155',
-        searchParams: {
-        }
+        pageCode: '623155'
     });
+
+  $('#deleteBtn').off("click").click(function () {
+    var selRecords = $('#tableList').bootstrapTable('getSelections');
+    if (selRecords.length <= 0) {
+      toastr.info("请选择记录");
+      return;
+    }
+
+    var msg = selRecords[0].status === '0' ? '确定注销该渠道？' : '确定激活该渠道？';
+    confirm(msg).then(function () {
+      reqApi({
+        code: '623253',
+        json: {
+          code: selRecords[0].code
+        }
+      }).then(function () {
+        sucList();
+      });
+
+    }, function () {
+    });
+
+  });
 
 });
